994c051b51c2eb9710bd7b4a5c2cb5bcc0f374f2
MagnavoxTG <dominik.lohner92@gmx.at>
2019-11-18 12:42:07 +0100

    Release 1.0

---
+++ cmake-build-release/CMakeFiles/i2bin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/i2bin.dir/main.c.o"
+  "i2bin"
+  "i2bin.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/i2bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
